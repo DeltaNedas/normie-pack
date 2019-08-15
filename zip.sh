310ab365b70e21d3a75cb5572f9791ca0cf67457
DeltaNedas <deltanedas@gmail.com>
2019-08-15 01:11:43 +0100

    1.1 - MODULAR POWERSUITS YEEEEEEEEEEEEEEEEEESSSSSSSSSSSSSS

--- zip.sh
+++ zip.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-zip -o normie-pack_1.0.zip overrides LICENSE manifest.json modlist.html README.md
+zip -o normie-pack_1.1.zip overrides LICENSE manifest.json modlist.html README.md
